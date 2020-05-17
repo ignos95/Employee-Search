4574b506c4e875c1605026fdaad5f3a5309ce9bf
ignos95 <ignos.c.a@gmail.com>
2020-05-17 22:01:47 +0530

    Updates

---
+++ precache-manifest.dab86c893d6ffd3f24dffa7c2a7a23ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf96f8b9ab4eb1846d7da8b89034431",
+    "url": "/Employee-Search/index.html"
+  },
+  {
+    "revision": "e145d8a06505f75c6e06",
+    "url": "/Employee-Search/static/css/main.a7d6f610.chunk.css"
+  },
+  {
+    "revision": "e40517f485db36a38937",
+    "url": "/Employee-Search/static/js/2.2f47512d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Search/static/js/2.2f47512d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e145d8a06505f75c6e06",
+    "url": "/Employee-Search/static/js/main.71c34068.chunk.js"
+  },
+  {
+    "revision": "04c222872705aff52dfe",
+    "url": "/Employee-Search/static/js/runtime-main.d5ce98e2.js"
+  }
+]);
